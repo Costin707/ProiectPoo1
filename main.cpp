071644aed34dad2d0567cb0a4af5ae4c2b3b61aa
Costin707 <costing_2006@yahoo.com>
2026-03-22 22:35:40 +0200

    Clasa user + cateva functii

--- main.cpp
+++ main.cpp
@@ -1,1 +1,47 @@
-pam
+#include <iostream>
+#include <string.h>
+#include <vector>
+
+class User {
+    const int id;
+    std::string name;
+    static long noUsers;
+    float height;
+    float weight;
+    char gender;
+    double BMI;
+    void copyName(const char* src);
+public:
+    User();
+    User(std::string name, float height, float weight, char gender);
+    void setHeight(float);
+    void setWeight(float);
+    void calculateBMI();
+};
+
+long User::noUsers = 0;
+
+
+
+User::User() : id(++noUsers) {
+    name = "N/A";
+    height = 0;
+    weight = 0;
+    gender = 'N';
+    BMI = 0;
+}
+void User::calculateBMI() {
+    if (height > 0 && weight > 0  )
+        BMI = weight / (height * height);
+    else
+        BMI = 0;
+}
+void User::setWeight(float weight) {
+    this->weight = weight;
+    calculateBMI();
+}
+
+void User::setHeight(float height) {
+    this->weight = height;
+    calculateBMI();
+}
